8ae58a0f38a4f050d5d56b58062831f806aaaf7d
Alex Wu <alexwu727@gmail.com>
2024-02-26 13:58:52 -0800

    use different rest templates with different profiles

---
+++ auth-system-backend/auth-system-authentication-service/src/main/java/io/github/alexwu727/authsystemauthenticationservice/config/RestConfig.java
@@ -0,0 +1,24 @@
+package io.github.alexwu727.authsystemauthenticationservice.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    @Profile("dev")
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    @Profile("!dev")
+    @LoadBalanced
+    public RestTemplate loadBalancedRestTemplate() {
+        return new RestTemplate();
+    }
+}
